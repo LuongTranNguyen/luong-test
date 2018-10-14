e17da86261715be3132dd553e0cfaeb1da6d2b8a
Luong Tran <luongtn265@gmail.com>
2018-10-14 10:27:35 +0700

    Signed-off-by: Luong Tran <luongtn265@gmail.com>

--- luong2-br2-file1.php
+++ luong2-br2-file1.php
@@ -1,8 +1,15 @@
 <?php
 
+$project1 = "New text from project 1";
+
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
+$project2 = "New text from project2";
+
 ?>
+
